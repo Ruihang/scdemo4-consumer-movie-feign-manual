971c280ddb8c9278055c801e66a952518c7d78be
行君 <ruihang@foxmail.com>
2017-11-23 23:56:34 +0800

    Initial commit

---
+++ src/main/java/com/ruix/scdemo4/MovieController.java
@@ -0,0 +1,27 @@
+package com.ruix.scdemo4;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author Ruixian
+ * @date 2017/11/14
+ */
+@RestController
+public class MovieController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    @GetMapping("/user/{id}")
+    public User findById(@PathVariable Long id) {
+        return this.userFeignClient.findById(id);
+    }
+
+}
---
+++ src/main/java/com/ruix/scdemo4/UserFeignClient.java
@@ -0,0 +1,16 @@
+package com.ruix.scdemo4;
+
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * @author Ruixian
+ * @date 2017/11/16
+ */
+@FeignClient(name = "scdemo2")
+public interface UserFeignClient {
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    User findById(@PathVariable("id") Long id);
+}
